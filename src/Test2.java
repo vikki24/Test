a3789c3fb38b24eda9af36b6cd189b27e174f653
Sabarish <Sabarish@DESKTOP-58N5152>
2020-06-30 12:50:54 +0530

    commiting to Branch X

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+	boolean a=false;
+		
+		if(a= false) {
+			System.out.println("print am true");
+		}
+		else {
+			System.out.println("print am false");
+		}
+	}
+
+}
